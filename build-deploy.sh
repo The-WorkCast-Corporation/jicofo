ebdaa81901c6bbefb2b181c5ad346de662f383e6
mortonprod <mortonprod@gmail.com>
2021-12-10 13:12:04 +0000

    Fix wrong target name

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,7 +1,7 @@
 set -e
 VERSION="test"
 PLAYBOOK_FILE="./jicofo-build.yml"
-LOCAL_BUILD="./target/jicofo-8.0-SNAPSHOT-jar-with-dependencies.jar"
+LOCAL_BUILD="./target/jicofo-1.1-SNAPSHOT-jar-with-dependencies.jar"
 REGION="us-east-1"
 TAG_TYPE="jicofo"
 TAG_CLIENT="trialist3"
